152798da0519f32141b7c53216845df43059434b
Murtaza01 <merdada2000@hotmail.com>
2024-09-20 16:09:05 +0300

    added two more books to second stage

--- src/assets/data/secondStage.js
+++ src/assets/data/secondStage.js
@@ -3,6 +3,9 @@ import phonetics from "../images/books/stage2/phonetics_and_phonology.jpg"
 import reading from "../images/books/stage2/select_reading.jpg"
 import drama from "../images/books/stage2/doctor_faustus.jpeg"
 import novel from "../images/books/stage2/joseph_andrews.jpg"
+import communication from "../images/books/stage2/person_to_person.jpg"
+import poetry from "../images/books/stage2/poetry.jpg"
+
 
 const secondStage = {
   books: [
@@ -11,7 +14,7 @@ const secondStage = {
       image: novel,
       link:"https://drive.google.com/file/d/11gAogLHlmQRO9Du33ljpnkZfsf_ogmO4/view?usp=drive_link", 
       subject: {
-        color: "bg-gradient-to-r from-stone-300  to-stone-500",
+        color: "bg-gradient-to-r from-rose-300  to-rose-500",
         subjectDes: "novel",
       },
     },
@@ -47,11 +50,29 @@ const secondStage = {
       image: drama,
       link: "https://drive.google.com/file/d/1JpvDX-7wMu6XYg9-mG2sK8rKUDL787q1/view?usp=drive_link",
       subject: {
-        color: "bg-gradient-to-r from-red-300  to-red-500",
+        color: "bg-gradient-to-r from-emerald-300  to-emerald-500",
         subjectDes: "drama",
       },
     },
- 
+ {
+      title: "communication",
+      image: communication,
+      link: "https://drive.google.com/file/d/13QY5R16OU1uUDsUfgYlfdcTBqrDXY4tH/view?usp=drive_link",
+      subject: {
+        color: "bg-gradient-to-r from-red-300  to-red-500",
+        subjectDes: "communication",
+      },
+    },
+{
+      title: "poetry",
+      image: poetry,
+      link: "https://drive.google.com/file/d/1ZiBDMeRpSFbTyJI8TfzTEo8g1hxmbgUD/view?usp=drive_link",
+      subject: {
+        color: "bg-gradient-to-r from-violet-300  to-violet-500",
+        subjectDes: "poetry",
+      },
+    },
+
   ],
 };
 
